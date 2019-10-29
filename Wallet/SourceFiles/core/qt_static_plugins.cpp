74dd2317764e88485a3368a363d0e165cb7887f2
John Preston <johnprestonmail@gmail.com>
2019-10-29 20:12:58 +0300

    Fix build for Linux.

--- Wallet/SourceFiles/core/qt_static_plugins.cpp
+++ Wallet/SourceFiles/core/qt_static_plugins.cpp
@@ -15,7 +15,6 @@ Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
 Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin)
 Q_IMPORT_PLUGIN(QGenericEnginePlugin)
 #elif defined Q_OS_LINUX // Q_OS_WIN | Q_OS_MAC
-Q_IMPORT_PLUGIN(QWebpPlugin)
 Q_IMPORT_PLUGIN(QXcbIntegrationPlugin)
 Q_IMPORT_PLUGIN(QConnmanEnginePlugin)
 Q_IMPORT_PLUGIN(QGenericEnginePlugin)
