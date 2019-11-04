98723690763df951e31701c325b5e1e6ac768825
John Preston <johnprestonmail@gmail.com>
2019-11-04 13:06:34 +0300

    Version 0.9.3.
    - Allow resizing the main window.
    - Bug fixes.

--- Wallet/SourceFiles/core/version.h
+++ Wallet/SourceFiles/core/version.h
@@ -6,5 +6,5 @@
 //
 #pragma once
 
-inline constexpr auto AppVersion = 9002;
-inline constexpr auto AppVersionStr = "0.9.2";
+inline constexpr auto AppVersion = 9003;
+inline constexpr auto AppVersionStr = "0.9.3";
