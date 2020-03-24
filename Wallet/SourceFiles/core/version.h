8e20e6baf66b9da78a83ab8a696a007b5cdef8e1
John Preston <johnprestonmail@gmail.com>
2020-03-24 21:43:09 +0400

    Version 0.9.5.
    - Update config for testnet2 in tonlib.

--- Wallet/SourceFiles/core/version.h
+++ Wallet/SourceFiles/core/version.h
@@ -6,5 +6,5 @@
 //
 #pragma once
 
-inline constexpr auto AppVersion = 9004;
-inline constexpr auto AppVersionStr = "0.9.4";
+inline constexpr auto AppVersion = 9005;
+inline constexpr auto AppVersionStr = "0.9.5";
