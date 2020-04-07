4e1c89d704f79dabb16608aebf3467a9b6ac3fed
John Preston <johnprestonmail@gmail.com>
2020-04-07 16:59:26 +0400

    Version 0.9.6.
    - Use encrypted transaction comments.

--- Wallet/SourceFiles/core/version.h
+++ Wallet/SourceFiles/core/version.h
@@ -6,5 +6,5 @@
 //
 #pragma once
 
-inline constexpr auto AppVersion = 9005;
-inline constexpr auto AppVersionStr = "0.9.5";
+inline constexpr auto AppVersion = 9006;
+inline constexpr auto AppVersionStr = "0.9.6";
