ff0a249099b0ff1368929ec972d6afce981a707b
John Preston <johnprestonmail@gmail.com>
2020-04-26 14:19:23 +0400

    Add support for two networks.

--- Wallet/SourceFiles/wallet/ton_default_settings.cpp
+++ Wallet/SourceFiles/wallet/ton_default_settings.cpp
@@ -14,13 +14,21 @@ namespace Wallet {
 
 Ton::Settings GetDefaultSettings() {
 	auto result = Ton::Settings();
-	auto file = QFile(":/config/test-default.json");
-	file.open(QIODevice::ReadOnly);
-	result.config = file.readAll();
+
+	auto test = QFile(":/config/test-default.json");
+	test.open(QIODevice::ReadOnly);
+	result.test.config = test.readAll();
+	result.test.blockchainName = "testnet2";
+	result.test.configUrl = "https://test.ton.org/config.json";
+
+	auto main = QFile(":/config/default.json");
+	main.open(QIODevice::ReadOnly);
+	result.main.config = main.readAll();
+	result.main.blockchainName = "mainnet";
+	result.main.configUrl = "https://ton.org/config.json";
+
 	result.useNetworkCallbacks = false;
-	result.blockchainName = "testnet2";
-	result.configUrl = "https://test.ton.org/config.json";
-	result.version = 2;
+	result.version = 3;
 	return result;
 }
 
